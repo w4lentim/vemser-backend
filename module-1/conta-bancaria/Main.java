caa35b1300b29d49293705a69dad6c6b48ac3ab7
Willian Valentim <williann.valentim@gmail.com>
2022-06-22 14:47:51 -0300

    Melhorado legibilidade do main

--- module-1/conta-bancaria/Main.java
+++ module-1/conta-bancaria/Main.java
@@ -4,16 +4,16 @@ public class Main {
         // Configurando dados para o cliente 1;
         Cliente cliente1 = new Cliente();
         cliente1.nome = "Willian Valentim";
-        cliente1.cpf = "12345678910";
+        cliente1.cpf = "123.456.789-10";
 
         Contato contato1 = new Contato();
         Contato contato2 = new Contato();
 
         contato1.descricao = "Telefone celular";
-        contato1.telefone = "85981828384";
+        contato1.telefone = "(85) 92223-4442";
         contato1.tipo = 1;
         contato2.descricao = "Telefone fixo";
-        contato2.telefone = "30333434";
+        contato2.telefone = "(85) 3333-3210";
         contato2.tipo = 2;
 
         cliente1.contatos[0] = contato1;
@@ -26,7 +26,7 @@ public class Main {
         endereco1.logradouro = "Rua Travessa Gama";
         endereco1.numero = 847;
         endereco1.complemento = "Próximo ao centro";
-        endereco1.cep = "12345678";
+        endereco1.cep = "12345-678";
         endereco1.cidade = "Maranguape";
         endereco1.estado = "Ceará";
         endereco1.pais = "Brasil";
@@ -35,7 +35,7 @@ public class Main {
         endereco2.logradouro = "Rua Travessa Quinderé";
         endereco2.numero = 123;
         endereco2.complemento = "Próximo a rodoviária";
-        endereco2.cep = "89101112";
+        endereco2.cep = "89101-112";
         endereco2.cidade = "Maranguape";
         endereco2.estado = "Ceará";
         endereco2.pais = "Brasil";
@@ -56,16 +56,16 @@ public class Main {
         // Configurando dados para o cliente 2;
         Cliente cliente2 = new Cliente();
         cliente2.nome = "Natanael Silva";
-        cliente2.cpf = "10111122233";
+        cliente2.cpf = "101.111.222-33";
 
         Contato contato11 = new Contato();
         Contato contato22 = new Contato();
 
         contato11.descricao = "Telefone celular";
-        contato11.telefone = "8591234567";
+        contato11.telefone = "(85) 91234-5567";
         contato11.tipo = 1;
         contato22.descricao = "Telefone fixo";
-        contato22.telefone = "31445566";
+        contato22.telefone = "(85) 3144-5566";
         contato22.tipo = 2;
 
         cliente2.contatos[0] = contato11;
@@ -78,7 +78,7 @@ public class Main {
         endereco11.logradouro = "Rua Raul Barbosa";
         endereco11.numero = 0101;
         endereco11.complemento = "Próximo ao centro";
-        endereco11.cep = "11223342";
+        endereco11.cep = "11223-342";
         endereco11.cidade = "Fortaleza";
         endereco11.estado = "Ceará";
         endereco11.pais = "Brasil";
@@ -87,7 +87,7 @@ public class Main {
         endereco22.logradouro = "Rua Padre Antonino";
         endereco22.numero = 1010;
         endereco22.complemento = "Próximo a Av. Antonio Sales";
-        endereco22.cep = "44556677";
+        endereco22.cep = "44556-677";
         endereco22.cidade = "Fortaleza";
         endereco22.estado = "Ceará";
         endereco22.pais = "Brasil";
@@ -106,24 +106,21 @@ public class Main {
         clienteConta2.imprimirContaCorrente(); // Imprimir dados de conta corrente do cliente 2;
 
         // Operando na conta do cliente 1;
-        cliente1.imprimirCliente();
-        cliente1.imprimirContatos();
-        cliente1.imprimirEnderecos();
         clienteConta1.imprimirContaCorrente();
+
         // Depósito e saque na conta do cliente 1;
         clienteConta1.deposito(200.00);
         clienteConta1.saque(100.00);
         System.out.printf("\nSaldo do Cheque Especial da conta R$: %.2f\n", clienteConta1.retornarSaldoComCheque());
+        System.out.printf("--------------------------------------\n");
 
         // Operando na conta do cliente 2;
-        cliente2.imprimirCliente();
-        cliente2.imprimirContatos();
-        cliente2.imprimirEnderecos();
         clienteConta2.imprimirContaCorrente();
         // Depósito e saque na conta do cliente 2;
         clienteConta2.deposito(1200.00);
         clienteConta2.saque(200.00);
         System.out.printf("\nSaldo do Cheque Especial da conta R$: %.2f\n", clienteConta2.retornarSaldoComCheque());
+        System.out.printf("--------------------------------------\n");
 
         // Efetuando transferência cliente 1 -> cliente 2;
         clienteConta1.transferir(clienteConta2, 200);
